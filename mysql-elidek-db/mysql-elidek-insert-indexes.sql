ea017c540bb2fe7a52e84b98b954938dc43fceae
TheodoraVrn <63544779+TheodoraVrn@users.noreply.github.com>
2022-06-05 19:08:51 +0300

    Add files via upload

---
+++ mysql-elidek-db/mysql-elidek-insert-indexes.sql
@@ -0,0 +1,4 @@
+create index idx_project_id on project(project_id);
+create index idx_researcher_id on researcher(researcher_id);
+create index idx_organisation_id on organisation(organisation_id);
+create index idx_executive_id on executive(executive_id);
